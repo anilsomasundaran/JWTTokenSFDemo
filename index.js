5d3674ffa3264f2fa5fe6dfa05c40c86f8e3b37f
anilsomasundaran <38529289+anilsomasundaran@users.noreply.github.com>
2021-06-09 18:45:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const helmet = require("helmet");
+const config = require("dotenv").config();
+const jwtLib = require("./lib/jwtlib");
+const apiLib = require('./lib/apilib');
+
+const app = express();
+app.use(helmet());
+
+const port = process.env.PORT;
+const host = process.env.HOST;
+const env = process.env.NODE_ENV;
+
+
+app.get('/', (req,res) => {
+    getToken();
+    res.send("Hello World");
+});
+
+async function getToken() {
+    let options = {
+        iss: process.env.CLIENT_ID,
+        sub: process.env.USER_NAME,
+        aud: process.env.URL,
+        privateKey: jwtLib.readPrivateKey()
+    }
+    //console.log(JSON.stringify(options));
+    const token = await jwtLib.getJWTToken(options);
+    const access_token = apiLib.requestToken(token);
+    const soql = "Select id name from Account".replaceAll(" ", "+");
+    console.log("Query => " + soql)
+    apiLib.query(access_token, soql);
+    //console.log(token);
+}
+
+getToken();
+
+app.listen(port, () => {
+    console.log(`server started at port : ${port}`);
+})
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+      console.log('Process terminated')
+    })
+})
